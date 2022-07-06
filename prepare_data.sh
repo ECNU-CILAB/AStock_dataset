d52079992d7fde396fca442fd428f3c7d97fcb54
ginXandXbitters <51900818+ginXandXbitters@users.noreply.github.com>
2022-07-07 00:21:28 +0800

    delete link

--- prepare_data.sh
+++ prepare_data.sh
@@ -7,10 +7,4 @@ cd datasets
 mkdir -p raw
 cd raw
 
-gdown "https://drive.google.com/uc?id=1PwWn9dNgczGyFySC5zj0rPp1qA4F04NW"
-gdown "https://drive.google.com/uc?id=1XSutgq2N7EhxethiFqnbjMZXhfQw_ZdS"
-gdown "https://drive.google.com/uc?id=18xQzXEuxAX4lG8uE_YcYM9_8e6ui7yQ1"
-gdown "https://drive.google.com/uc?id=18nA5gpgVUsldJTsP42_uCrIujXJX78h1"
-gdown "https://drive.google.com/uc?id=1dfeCd5Thhu6kpfgVX3_1xFFIZPG4S2uu"
-
 
